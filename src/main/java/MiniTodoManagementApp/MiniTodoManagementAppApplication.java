861c69126be4d7845b54e49a9d057d265cf42d1a
rafal1018 <rafaljaneczek@o2.pl>
2021-05-04 20:36:06 +0200

    Initial commit

---
+++ src/main/java/MiniTodoManagementApp/MiniTodoManagementAppApplication.java
@@ -0,0 +1,13 @@
+package MiniTodoManagementApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MiniTodoManagementAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MiniTodoManagementAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/MiniTodoManagementApp/MiniTodoManagementAppApplicationTests.java
@@ -0,0 +1,13 @@
+package MiniTodoManagementApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MiniTodoManagementAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
